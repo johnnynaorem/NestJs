a39a8b0e10eade229fe8aabea76a571b7adf6b7a
johnnynaorem <johnnynaorem7@gmail.com>
2025-02-28 16:56:55 +0530

    Add Product Filter Features

--- src/app.service.ts
+++ src/app.service.ts
@@ -24,4 +24,50 @@ export class AppService {
     const index = this.products.findIndex((p) => p.id === id);
     return this.products.splice(index, 1);
   }
+
+  UpdateProduct(id: number, entity: Product): boolean {
+    const isExist: boolean = this.products.find((p) => p.id == id)
+      ? true
+      : false;
+    console.log(isExist);
+    if (!isExist) {
+      return false;
+    }
+    return true;
+  }
+
+  FilterProduct(query: any) {
+    let { category, name, price } = query;
+    if (name && category && price) {
+      return this.products.filter(
+        (p) =>
+          p.name.toLowerCase().includes(name.toLowerCase()) &&
+          p.category === category &&
+          p.price > +price,
+      );
+    } else if (name && category) {
+      return this.products.filter(
+        (p) =>
+          p.name.toLowerCase().includes(name.toLowerCase()) &&
+          p.category === category,
+      );
+    } else if (price && category) {
+      return this.products.filter(
+        (p) => p.price > +price && p.category === category,
+      );
+    } else if (price && name) {
+      return this.products.filter(
+        (p) =>
+          p.name.toLowerCase().includes(name.toLowerCase()) && p.price > +price,
+      );
+    } else if (name) {
+      return this.products.filter((p) =>
+        p.name.toLowerCase().includes(name.toLowerCase()),
+      );
+    } else if (category) {
+      return this.products.filter((p) => p.category === category);
+    } else if (price) {
+      return this.products.filter((p) => p.price > price);
+    }
+  }
 }
