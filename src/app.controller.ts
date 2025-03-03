fc0051d3738462b9ce817282eab7c77bc9b62f1f
johnnynaorem <johnnynaorem7@gmail.com>
2025-03-03 12:40:04 +0530

    Add ProductModule

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,47 +1,11 @@
-import {
-  Body,
-  Controller,
-  Delete,
-  Get,
-  HttpCode,
-  Param,
-  Post,
-  Put,
-  Query,
-} from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-import { Product, ProductReturnDTO } from './models/Product.model';
 
-@Controller('products')
+@Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
-  @Get('/getProducts')
-  getProducts(): ProductReturnDTO {
-    return this.appService.GetAllProduct();
-  }
-
-  @Get('/filter')
-  search(@Query() query: any) {
-    return this.appService.FilterProduct(query);
-  }
-
-  @Post('/create-product')
-  createProduct(@Body() product: Product): string {
-    return this.appService.CreateProduct(product);
-  }
-
-  @Delete('/delete/:id')
-  deleteProduct(@Param('id') id: number): Product[] {
-    return this.appService.DeleteProduct(id);
-  }
-
-  @Put('/update/:id')
-  updateProduct(@Param('id') id: number, @Body() product: Product) {
-    const res = this.appService.UpdateProduct(id, product);
-    if (res) {
-      return res;
-    }
-    return res;
+  @Get()
+  GetString(): string {
+    return this.appService.getMsg();
   }
 }
